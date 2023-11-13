8f5d541af9e485b5f11a440e5040321d3c5e1a71
Andres Saavedra <and318@MacBook-Air-de-Andres.local>
2023-11-13 15:01:26 -0300

    Add axios and pokeApi rest

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+searchPokemon() {
+throw new Error('Method not implemented.');
+}
   title = 'pokedex';
+  pokemonName: string | undefined;
+  pokeImage: string | undefined;
+  pokemonNumber: number | undefined;
+
+  ngOnInit() {
+    this.getPokemonData(14);
+  }
+
+  getPokemonData(pokemonNumber: number) {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
+      .then((response) => {
+        this.pokemonName = response.data.name;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
